77aae48d127bbbe8f9d7a99fe1e339bcf2808eb0
AdriCQ <adrian.capote95@zoho.com>
2021-09-28 19:51:52 -0400

    Add App http protection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
+/**
+ *
+ */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Protection
+  app.use(compression());
+  app.use(helmet());
+  app.use(csurf());
+
   await app.listen(3000);
 }
 bootstrap();
