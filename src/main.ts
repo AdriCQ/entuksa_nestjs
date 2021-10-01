1d7b47fdeb6ed5e857941481c5d6be98e1685a09
AdriCQ <adrian.capote95@zoho.com>
2021-10-01 14:10:10 -0400

    Auto validation

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
 import * as helmet from 'helmet';
-import * as csurf from 'csurf';
+// import * as csurf from 'csurf';
+import { ValidationPipe } from '@nestjs/common';
 /**
  *
  */
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   // Protection
   app.use(compression());
   app.use(helmet());
